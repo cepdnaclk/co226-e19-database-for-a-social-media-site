486177d773e233a6f9c9d88cafde804b91176dfe
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-06-09 07:09:54 +0530

    changed the sign up (remove unnessesery fields

--- backend/signup.js
+++ backend/signup.js
@@ -1,69 +1,48 @@
 ////////////////////////// signup.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
 // Define a route for user sign up with database connection
-const route = (db)=>{
-router.post('/', (req, res) => {
-    const {
-      user_name,
-      email,
-      password,
-      confirmPassword,
-      first_name,
-      last_name,
-      b_date,
-      b_month,
-      b_year,
-      sex
-    } = req.body;
-  
+const route = (db) => {
+  router.post("/", (req, res) => {
+    const { user_name, email, password, confirmPassword } = req.body;
+
     // Check if username is already taken
     const usernameQuery = `SELECT * FROM user WHERE user_name = ?`;
-  
+
     db.query(usernameQuery, [user_name], (error, results) => {
       if (error) {
         // Handle error
-        console.error('Error checking username:', error);
-        res.status(500).json({ error: 'Error checking username' });
+        console.error("Error checking username:", error);
+        res.status(500).json({ error: "Error checking username" });
       } else {
         if (results.length > 0) {
           // Username is already taken
-          res
-            .status(400)
-            .json({ error: 'Username is already taken. Please enter a different username' });
+          res.status(400).json({
+            error:
+              "Username is already taken. Please enter a different username",
+          });
         } else {
           // Username is valid, proceed to store in the database
-          
-          const addUserQuery = `INSERT INTO user (user_name, email, password, first_name, last_name, b_date, b_month, b_year, sex) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-  
+
+          const addUserQuery = `INSERT INTO user (user_name, email, password) VALUES (?, ?, ?)`;
+
           // Check if passwords match
           if (password !== confirmPassword) {
-            res.status(400).json({ error: 'Passwords do not match' });
+            res.status(400).json({ error: "Passwords do not match" });
           } else {
             // Execute the query to store user data
             db.query(
               addUserQuery,
-              [
-                user_name,
-                email,
-                password,
-                first_name,
-                last_name,
-                b_date,
-                b_month,
-                b_year,
-                sex
-              ],
+              [user_name, email, password],
               (error, result) => {
                 if (error) {
                   // Handle error
-                  console.error('Error adding user:', error);
-                  res.status(500).json({ error: 'Error adding user' });
+                  console.error("Error adding user:", error);
+                  res.status(500).json({ error: "Error adding user" });
                 } else {
                   // User data stored successfully
-                  res.json({ message: 'User registration successful' });
+                  res.json({ message: "User registration successful" });
                 }
               }
             );
