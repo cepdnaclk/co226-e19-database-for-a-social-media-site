7f9f5029ec0d04372365e59147cabc20d9ab5f73
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-08-02 12:13:02 +0530

    made friends view

--- backend/search_friend.js
+++ backend/search_friend.js
@@ -10,13 +10,36 @@ const route = (db) => {
 
     // Retrieve friend profiles from the 'user' table based on the search query
     const query = `
-    SELECT u.user_name,u.first_name, u.last_name, u.email, u.location, u.affiliation, u.bio, u.interest
+    SELECT u.user_name,u.first_name, u.last_name, u.email, u.location, u.affiliation, u.bio, u.interest, u.profile_picture
     FROM user AS u
     JOIN friends_with AS f ON ( u.u_id = f.accepter_id OR u.u_id = f.requester_id )
     WHERE (f.requester_id = ? OR f.accepter_id = ? ) 
   `;
     const searchValue = `%${searchQuery}%`;
     db.query(query, [u_id, searchValue, searchValue], (err, results) => {
+      if (err) {
+        console.error(
+          "Error retrieving friend profiles from the database:",
+          err
+        );
+        res.status(500).json({ error: "Failed to retrieve friend profiles" });
+      } else {
+        const profiles = results;
+
+        res.json(profiles);
+      }
+    });
+  });
+  router.get("/all", (req, res) => {
+    const u_id = req.query.u_id; // user ID is passed as a query parameter
+
+    // Retrieve friend profiles from the 'user' table based on the search query
+    const query = `
+    SELECT u.user_name,u.first_name, u.last_name, u.email, u.location, u.affiliation, u.bio, u.interest, u.profile_picture
+    FROM user AS u
+    JOIN friends_with AS f ON ( u.u_id = f.accepter_id OR u.u_id = f.requester_id )
+  `;
+    db.query(query, [u_id], (err, results) => {
       if (err) {
         console.error(
           "Error retrieving friend profiles from the database:",
--- frontend/src/views/main/DashView.vue
+++ frontend/src/views/main/DashView.vue
@@ -6,7 +6,15 @@
                     <comp-post v-for="post in posts" :key="post.id" :post="post" />
                 </div>
                 <div class="comm-deck">
-                    <comp-comment :comment="comment" />
+                    <div class="friends">
+                        <h2>Friends</h2>
+                        <router-link :to="`profile/${friend.user_name}`" class="profile" v-for="friend in friends"
+                            :key="friend.user_name">
+                            <img src="" alt="">
+                            <h4>{{ friend.first_name + " " + friend.last_name }}</h4>
+                            <p>@{{ friend.user_name }}</p>
+                        </router-link>
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,10 +28,21 @@ import compComment from '@/components/compComment.vue';
 import { onMounted, ref } from 'vue';
 
 const posts = ref([])
+const friends = ref([])
 
 const getPosts = async () => {
     try {
-        const result = await axios.get(`/post_feed/?u_id=${1}`);
+        const result = await axios.get(`/post_feed/?u_id=${2}`);
+        return result.data
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+const getFriends = async () => {
+    try {
+        const result = await axios.get(`/search_friend/?u_id=${2}`);
         console.log(result.data)
         return result.data
     }
@@ -34,14 +53,9 @@ const getPosts = async () => {
 
 onMounted(async () => {
     posts.value = await getPosts()
+    friends.value = await getFriends()
 })
 
-const comment = ref({
-    name: "funkybird",
-    content: "asdfsd kfhkj sdfads fasdf",
-    no_likes: 12,
-    time: "26 May",
-})
 </script>
 
 <style setup>
@@ -60,10 +74,51 @@ const comment = ref({
 }
 
 .comm-deck {
+    position: fixed;
+    right: 0;
+    height: 100vh;
     width: 40%;
     min-width: 300px;
     padding: 2rem 1rem;
     background: white;
+}
+
+.friends h2 {
+    margin-bottom: 2rem;
+}
+
+.friends .profile {
+    display: grid;
+    grid-template-columns: 3rem auto;
+    grid-template-rows: 1fr 1fr;
+    width: auto;
+    margin: 1rem 0;
+    text-decoration: none;
+}
+
+.friends .profile img {
+    grid-column: 1;
+    grid-row: 1/3;
+    height: 3rem;
+    width: 3rem;
+    border-radius: 50%;
+    border: 2px solid #2FA634;
+    object-fit: cover;
+}
+
+.friends .profile h4 {
+    font-size: 1rem;
+    grid-column: 2;
+    grid-row: 1;
+    margin-left: 0.75rem;
+    margin-top: auto;
+    color: black;
+}
+
+.friends .profile p {
+    font-size: 0.8rem;
+    margin-left: 0.75rem;
+    color: #555;
 }
 
 @media screen and (max-width: 769px) {
