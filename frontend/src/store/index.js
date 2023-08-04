8bd1145663cf8509d35dc9c5c1ab1bb2cb663e9c
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-08-04 17:05:47 +0530

    change store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,35 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    sucList: [],
+    errList: [],
+    isAuthenticated: false,
+    token: "",
+    user: Object,
+    currentSignupUser: "",
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    addSuccess(state, msg) {
+      state.sucList.push(msg);
+      setTimeout(() => {
+        state.sucList.splice(0, 1);
+      }, 2000);
+    },
+    addError(state, msg) {
+      state.errList.push(msg);
+      setTimeout(() => {
+        state.errList.splice(0, 1);
+      }, 2000);
+    },
+    setLogin(state, payload) {
+      state.token = payload.token;
+      state.isAuthenticated = true;
+      state.user = payload.user;
+      axios.defaults.headers = { Authorization: payload.token };
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
