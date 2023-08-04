fe1261b14451d56a3fca605d01d7e1fbfab45383
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-08-04 17:04:44 +0530

    create alert components

---
+++ frontend/src/components/compErrorAlert.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="alert alert-danger">
+        <strong class="me-1">Error!</strong> {{ props.msg }}
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['msg'])
+</script>
+
+<style scoped>
+.alert {
+    position: fixed;
+    top: 5rem;
+    left: 50%;
+    transform: translate(-50%);
+    z-index: 100;
+    width: 60%;
+    min-width: 300px;
+    color: white;
+    background: red;
+    padding: 1rem 2rem;
+    border-radius: 0.5rem;
+}
+</style>
