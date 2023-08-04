209bbbcaa703b11fe4bda6407f4d8397e9fe1b0e
KalinduWijerathna <e19446@eng.pdn.ac.lk>
2023-08-04 11:51:46 +0530

    correction in post.js

--- backend/post.js
+++ backend/post.js
@@ -1,3 +1,5 @@
+///////////////////////////////// post.js
+
 const express = require("express");
 const router = express.Router();
 
@@ -6,7 +8,7 @@ const route = (db) => {
 
 // Middleware to check if the user exists
 const userExists = (req, res, next) => {
-  const userId = req.body.userid; // Assuming the user ID is provided in the request body
+  const userId = req.body.u_id; // Assuming the user ID is provided in the request body
   db.query("SELECT u_id FROM user WHERE u_id = ?", [userId], (err, results) => {
     if (err) {
       return res.status(500).json({ error: "Database error" });
@@ -20,7 +22,7 @@ const userExists = (req, res, next) => {
 
 // POST route to add a new post
 router.post("/add", userExists, (req, res) => {
-  const userId = req.body.userid; // Assuming the user ID is provided in the request body
+  const userId = req.body.u_id; // Assuming the user ID is provided in the request body
   const { p_title, p_time, p_date, p_month, p_year, content } = req.body;
   const post = {
     p_title,
@@ -29,7 +31,7 @@ router.post("/add", userExists, (req, res) => {
     p_month,
     p_year,
     content,
-    user_id: userId,
+    u_id: userId,
   };
   db.query("INSERT INTO post SET ?", post, (err, results) => {
     if (err) {
@@ -41,8 +43,8 @@ router.post("/add", userExists, (req, res) => {
 
 // DELETE route to delete a post
 router.delete("/delete/:postid", userExists, (req, res) => {
-  const postId = req.params.postid; // Assuming the post ID is provided as a route parameter
-  const userId = req.body.userid; // Assuming the user ID is provided in the request body
+  const postId = req.params.p_id; // Assuming the post ID is provided as a route parameter
+  const userId = req.body.u_id; // Assuming the user ID is provided in the request body
   db.query("SELECT user_id FROM post WHERE p_id = ?", [postId], (err, results) => {
     if (err) {
       return res.status(500).json({ error: "Database error" });
@@ -50,7 +52,7 @@ router.delete("/delete/:postid", userExists, (req, res) => {
     if (results.length === 0) {
       return res.status(404).json({ error: "Post not found" });
     }
-    const postUserId = results[0].user_id;
+    const postUserId = results[0].u_id;
     if (postUserId !== userId) {
       return res.status(403).json({ error: "You are not authorized to delete this post" });
     }
